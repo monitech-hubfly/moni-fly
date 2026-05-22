0b5d839c50311f3f178ca064abf318c6a35e85ff
Danilo Nyitray <danilo.n@moni.casa>
2026-05-22 16:13:26 -0300

    fix: remove pasta src/pages com arquivos React legados que quebravam o build

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Início</h1>
-      <p>Use o menu ao lado para acessar <strong>Áreas</strong>, <strong>Workload</strong> e <strong>Carômetro</strong>.</p>
-    </>
-  )
-}
