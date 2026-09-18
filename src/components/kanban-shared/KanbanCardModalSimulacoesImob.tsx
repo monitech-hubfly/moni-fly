740b719dbf2860346d58ab5c484ce8a7af53bd5a
Fernanda Lobão <fernanda.lobao@moni.casa>
2026-09-18 16:01:32 -0300

    fix: isDirty no showroom bloco

--- src/components/kanban-shared/KanbanCardModalSimulacoesImob.tsx
+++ src/components/kanban-shared/KanbanCardModalSimulacoesImob.tsx
@@ -1056,6 +1056,7 @@ export function KanbanCardModalSimulacoesImob({
                 ofertasExistentes={[]}
                 excluindoOfertaId={null}
                 onExcluirOferta={() => {}}
+                isDirty={false}
               />
             ))}
           </div>
